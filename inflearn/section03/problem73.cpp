b819930af0615baaf82b4ad15acc2c22f364916f
sjpark-dev <sjpark.dev@gmail.com>
2021-01-18 19:36:18 +0900

    add section03 codes

---
+++ inflearn/section03/problem73.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int pq[100000];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    priority_queue<int> pq;
+    while (true) {
+        cin >> n;
+        if (n==-1) {
+            break;
+        } else if (n==0) {
+            if (!pq.empty()) {
+                cout << pq.top() << "\n";
+                pq.pop();
+            } else cout << -1 << "\n";
+        } else pq.push(n);
+    }
+    return 0;
+}
