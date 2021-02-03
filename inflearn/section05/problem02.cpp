fb01f27ff16b8341478cd49ebfd9f23791cf1095
sjpark-dev <sjpark.dev@gmail.com>
2021-02-03 19:26:59 +0900

    add section05 codes

---
+++ inflearn/section05/problem02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int dy[46];
+
+int dfs(int n) {
+    if (dy[n]) return dy[n];
+    if (n==1 || n ==2) return n;
+    return dy[n] = dfs(n-2) + dfs(n-1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << dfs(n);
+    return 0;
+}
