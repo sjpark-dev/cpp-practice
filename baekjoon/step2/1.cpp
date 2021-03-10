1d547f72a897a9a30d3b74ff6f34c1aa11087c74
sjpark-dev <sjpark.dev@gmail.com>
2021-03-10 13:21:03 +0900

    update step2 codes

--- baekjoon/step2/1.cpp
+++ baekjoon/step2/1.cpp
@@ -4,10 +4,13 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int a, b;
     cin >> a >> b;
-    if (a>b) cout << ">";
-    else if (a==b) cout << "==";
+
+    if (a > b) cout << ">";
+    else if (a == b) cout << "==";
     else cout << "<";
+
     return 0;
 }
