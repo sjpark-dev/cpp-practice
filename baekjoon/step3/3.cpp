e1de86a8e425341ddd8cacce6170f60634125de3
sjpark-dev <sjpark.dev@gmail.com>
2021-03-14 00:11:28 +0900

    add step3 codes

---
+++ baekjoon/step3/3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, sum = 0;
+    cin >> n;
+    
+    for (int i=1; i<=n; i++) {
+        sum += i;
+    }
+
+    cout << sum;
+
+    return 0;
+}
